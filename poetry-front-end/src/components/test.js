57b49d2c44ec0951c1c0aab4064b773ee79751d4
KingHenryJr <henryyihengzhang@gmail.com>
2018-05-29 13:54:30 -0400

    routes

---
+++ poetry-front-end/src/components/test.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Test extends Component {
+
+  render(){
+    return(
+      <h1> Put Poems Here </h1>
+    )
+
+  }
+
+}
--- poetry-front-end/src/index.js
+++ poetry-front-end/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom'
+import Test from './components/test'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+
+  <Router>
+    <div>
+      <Route exact path="/" component={Test} />
+      <Route exact path="/poems" component={App} />
+    </div>
+  </Router>
+), document.getElementById('root'));
 registerServiceWorker();
